eeb5e60da0c765e65b7886c7a2fc2c19642280cc
Mark Burton <markb@smartavionics.com>
2020-03-17 15:00:17 +0000

    Revert "Set default e-steps to 413."
    This reverts commit 1efed1eff01e8c431c3d57e2ae0c89d984f4b192.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -735,8 +735,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 382.14 }
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 413 } // mb
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 382.14 }
 
 /**
  * Default Max Feed Rate (mm/s)
